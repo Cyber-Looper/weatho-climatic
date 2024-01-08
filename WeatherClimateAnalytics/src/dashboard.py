42778ad8712c77c50dd149baf891fea1fdf0bb28
Cyber_Looper <sudharcha14@gmail.com>
2024-01-08 14:56:15 +0530

    source updated

--- WeatherClimateAnalytics/src/dashboard.py
+++ WeatherClimateAnalytics/src/dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import geopandas as gpd
 from osgeo import gdal
 import warnings
