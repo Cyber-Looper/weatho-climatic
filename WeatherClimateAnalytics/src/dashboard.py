b369ac0f2c9bdb56b97a3fe03074ef9d3c5be98e
Cyber_Looper <sudharcha14@gmail.com>
2024-01-08 12:55:23 +0530

    source updated

--- WeatherClimateAnalytics/src/dashboard.py
+++ WeatherClimateAnalytics/src/dashboard.py
@@ -22,7 +22,7 @@ st.write(':snowflake: **Weather** and **Climate** play a pivotal role in shaping
          'wind speed, sea-level pressure, solar radiation, precipitation, and snowfall***.')
 
 # Dataset
-data = pd.read_csv('/mount/src/weatho-climatic/WeatherClimateAnalytics/dataset/upd_fecast_dta.csv')
+data = pd.read_csv('../dataset/upd_forecast_data.csv')
 # st.dataframe(data)
 
 # SideBar
