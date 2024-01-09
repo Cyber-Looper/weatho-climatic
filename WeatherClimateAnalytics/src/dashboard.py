76f098f6d1c0ef9aae641311479e7fc0cb7543c1
Cyber_Looper <sudharcha14@gmail.com>
2024-01-09 11:51:32 +0530

    source updated

--- WeatherClimateAnalytics/src/dashboard.py
+++ WeatherClimateAnalytics/src/dashboard.py
@@ -28,7 +28,7 @@ st.write(':snowflake: **Weather** and **Climate** play a pivotal role in shaping
 
 # Dataset
 # data = pd.read_csv('/mount/src/weatho-climatic/WeatherClimateAnalytics/dataset/upd_forecast_data.csv')
-data = pd.read_csv('../dataset/upd_forecast_data.csv')
+data = pd.read_csv('/mount/src/weatho-climatic/WeatherClimateAnalytics/dataset/upd_forecast_data.csv')
 # st.dataframe(data)
 
 # SideBar
