5f5e57ab33e2812b4ec068f28774a256345413a8
Cyber_Looper <sudharcha14@gmail.com>
2024-01-10 10:52:04 +0530

    source updated

--- WeatherClimateAnalytics/src/dashboard.py
+++ WeatherClimateAnalytics/src/dashboard.py
@@ -137,7 +137,7 @@ filtered_data_date = filtered_data.copy()
 
 # Timeline to filter Air Temperature
 aply_fil = 'Apply Timeline'
-st.write('**Apply Timeline**')
+
 st.markdown(f"""
         <h5>{aply_fil}</h5>
     """, unsafe_allow_html=True)
