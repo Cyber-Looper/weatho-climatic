ed4c4e09017f59671e98a5bb9f1f7a8006e0574d
Cyber_Looper <sudharcha14@gmail.com>
2024-01-10 11:27:45 +0530

    source updated

--- WeatherClimateAnalytics/src/dashboard.py
+++ WeatherClimateAnalytics/src/dashboard.py
@@ -159,6 +159,9 @@ filtered_date = filtered_data_date[
     (filtered_data_date["DATE_VALID_STD"] >= date1) & (filtered_data_date["DATE_VALID_STD"] <= date2)].copy()
 # st.write(filtered_date)
 
+# Sort the DataFrame by the date column in ascending order
+filtered_data_date.sort_values(by='DATE_VALID_STD', inplace=True)
+
 if selected_option == "AVG_TEMPERATURE_AIR_2M_F":
     linechart = pd.DataFrame(filtered_date.groupby((filtered_date["DATE_VALID_STD"].dt.strftime("%Y %b")))[
                                  "AVG_TEMPERATURE_AIR_2M_F"].sum()).reset_index()
